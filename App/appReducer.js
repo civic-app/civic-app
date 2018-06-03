97c7e0990b83706b24a31cc2d0c6e98b72117815
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-06-03 12:52:53 -0400

    Hook redux and sagas into app, change app structure to account for bootstrapping files

---
+++ App/appReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import authReducer, { AUTH_NAMESPACE } from '../src/auth/redux'
+
+const appReducer = combineReducers({
+  [AUTH_NAMESPACE]: authReducer,
+})
+
+export default appReducer
---
+++ App/appSagas.js
@@ -0,0 +1,10 @@
+import { all } from 'redux-saga/effects'
+import authSaga from '../src/auth/sagas'
+
+const appSaga = function* () {
+  yield all([
+    authSaga()
+  ])
+}
+
+export default appSaga
