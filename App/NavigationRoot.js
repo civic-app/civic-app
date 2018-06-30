1801f0d30fb26a99443716e4a83c47c6658c0d6c
gmferland <gmferland@gmail.com>
2018-06-30 15:28:24 -0400

    actually don't need Expo

--- App/NavigationRoot.js
+++ App/NavigationRoot.js
@@ -1,10 +1,15 @@
-import Expo from 'expo';
 import { createSwitchNavigator } from 'react-navigation';
 
 import AuthStack from '../src/navigation/AuthStack';
 import MainTabNavigator from '../src/navigation/MainTabNavigator';
 import SurveyStack from '../src/navigation/SurveyStack';
 
+/*
+ * Combines unrelated flows into one single app. For example,
+ * a user who has logged in and sees the survey should not be able
+ * to navigate back to the login screen. Eventually, we will use redux
+ * state to determine which initial route to show the user.
+ */
 const NavigationRoot = createSwitchNavigator(
   {
     App: MainTabNavigator,
@@ -15,7 +20,5 @@ const NavigationRoot = createSwitchNavigator(
     initialRouteName: 'Auth',
   },
 );
-
-Expo.registerRootComponent(NavigationRoot);
 
 export default NavigationRoot;
