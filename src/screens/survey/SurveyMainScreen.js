bc779980805c2053df5ff9c8e96333809120fb01
156javalos <156javalos@gmail.com>
2018-07-07 12:03:35 -0700

    Update on survey so far

--- src/screens/survey/SurveyMainScreen.js
+++ src/screens/survey/SurveyMainScreen.js
@@ -1,27 +1,91 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Button } from 'react-native';
+import { Text, View, Button,StyleSheet,TouchableWithoutFeedback, Image, Easing , Animated} from 'react-native';
+import {createStackNavigator, NavigationActions} from 'react-navigation';
+import Survey_start from './Survey_Start';
+import Question1 from './Question1';
+import Question6 from './Question6';
 
-import styles from '../styles';
+var RootStack = createStackNavigator(
+  {
+    Home: Survey_start,
+    Question1: Question1,
+    Question6: Question6
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: 'none',
+    cardStyle : {
+      backgroundColor: 'transparent'
+    }
+  }
+);
 
 class SurveyMainScreen extends React.Component {
-  static propTypes = propTypes;
-  goToApp = () => this.props.navigation.navigate('App');
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button title={'Go to App'} onPress={this.goToApp} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+
+        {/* This is the first row */}
+
+        <View style={styles.container}>
+          <View style={[styles.survey_block,{
+
+          }]}>
+            {/* Here goes the things */}
+            <RootStack/>
+          </View>
+        </View>
+
       </View>
     );
   }
 }
 
-const propTypes = {
-  navigation: PropTypes.objectOf({
-    navigate: PropTypes.func,
-    push: PropTypes.func,
-  }),
-};
+const styles = StyleSheet.create({
+  container: {
+    //Combined the two container styles so they didn't override each other
+    paddingTop: 20,
+    alignItems: 'center',
+    flex: 7,
+    //Aligned items flex-start so that they go to the top of the screen (leaving room for survey progress, etc)
+    justifyContent: 'flex-start',
+    //Grey
+    backgroundColor: '#E5E5E5'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'blue'
+  },
+
+  appbar_text: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontSize: 20,
+    lineHeight: 23,
+    marginTop: 45,
+    marginLeft: 50,
+    color: 'white'
+  },
+
+  //The white rectangle that holds the questions
+  survey_block: {
+    //originally 360, 550, borderwidth 50
+    width: 330,
+    height: 513,
+    backgroundColor: 'transparent'
+  }
+})
 
 export default SurveyMainScreen;
---
+++ src/screens/survey/Survey_Start.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import {createStackNavigator} from 'react-navigation';
+import { Text, View, Button, StyleSheet, TouchableWithoutFeedback, Image} from 'react-native';
+
+class Survey_start extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.survey_block}>
+
+        <View style={styles.block1}>
+          <Text style={[styles.font_style, styles.title] }>We'll ask a few questions {"\n"}to get to know the issues {"\n"}you care about.</Text>
+        </View>
+        <View style={styles.block2}>
+          <Text style={[styles.font_style]}>This will take 3 minutes and will {"\n"}help us find candidates and organizations that represent you.</Text>
+        </View>
+
+        <View style= {styles.image_block}>
+          <Image
+              style={styles.image}
+              source={require('./lib/noun_1696115_cc.png')}
+            />
+        </View>
+
+        <View >
+          <TouchableWithoutFeedback
+            onPressIn = {() => {this.props.navigation.navigate('Question1')}} >
+          <View style={styles.get_started_button}><Text style={styles.get_started_text}>GET STARTED</Text></View>
+          </TouchableWithoutFeedback>
+        </View>
+
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  // General survey block style
+  survey_block: {
+    width: 330,
+    height: 519,
+    backgroundColor: 'white',
+    overflow: 'hidden',
+    alignItems: 'center',
+    borderWidth: 10,
+    borderColor: 'white',
+    borderRadius: 20,
+    position: 'absolute',
+    //Shadow stuff
+    shadowColor: '#E5E5E5',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+  },
+
+  // Each block dividing the sections
+  block1:{
+    marginTop:32,
+    height:107,
+    width: 276,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  block2:{
+    height: 64,
+    width: 279,
+    marginTop:9,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image_block: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 35,
+    marginBottom:41.54,
+    backgroundColor: 'white'
+  },
+
+  // Style for general font
+  font_style: {
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    textAlign: 'justify'
+  },
+
+  // Style for independent items
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'justify'
+  },
+  image: {
+    width: 155,
+    height: 154
+  },
+  get_started_button: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  get_started_text: {
+    color: '#126390',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+})
+
+export default Survey_start;
