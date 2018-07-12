98c8d415d3ba3ff9a37e02298a3a774c25425b24
gmferland <gmferland@gmail.com>
2018-07-12 00:00:13 -0400

    the docs say I need to do this

--- src/welcome/WelcomePanel.js
+++ src/welcome/WelcomePanel.js
@@ -17,6 +17,7 @@ var styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
     marginBottom: 20,
+    borderRadius: 3,
     shadowColor: colors.black,
     shadowOffset: {
       width: 0,
