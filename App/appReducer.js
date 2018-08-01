e7a5b357244a8a25ecd44e4bbd22a2fa1d3cb346
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-08-01 00:09:12 -0400

    Reducer and sagas for favorites

--- App/appReducer.js
+++ App/appReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import authReducer, { AUTH_NAMESPACE } from '../src/auth/redux'
 import candidateReducer, { CANDIDATE_NAMESPACE } from '../src/candidate/redux/candidates'
 import positionsReducer, { POSITIONS_NAMESPACE } from '../src/candidate/redux/positions'
+import favoritesReducer, { FAVORITES_NAMESPACE } from '../src/favorites/redux';
 
 const appReducer = combineReducers({
   [AUTH_NAMESPACE]: authReducer,
   [CANDIDATE_NAMESPACE]: candidateReducer,
   [POSITIONS_NAMESPACE]: positionsReducer,
-})
+  [FAVORITES_NAMESPACE]: favoritesReducer,
+});
 
 export default appReducer;
---
+++ src/favorites/sagas.js
@@ -0,0 +1,27 @@
+import { call, select, put } from 'redux-saga/effects';
+import { getLoggedInUserId } from '../auth/redux';
+import { favoritesRequestSuccess, getIsFavorite, addOrRemoveFavorite } from './redux';
+import { fetchFavorites, updateFavorite } from './api';
+
+export const toggleFavoriteSaga = function*(action) {
+  try {
+    const userId = yield select(getLoggedInUserId);
+    const oldIsFavorite = yield select(getIsFavorite, action.payload.id, action.payload.category);
+    yield(put(addOrRemoveFavorite(action.payload.id, action.payload.category, !oldIsFavorite)));
+    yield call(updateFavorite, userId, action.payload.id, !oldIsFavorite, action.payload.category);
+  } catch (err) {
+    // TODO: display error message about failure
+    yield put(addOrRemoveFavorite(action.payload.id, action.payload.category));
+
+  }
+};
+
+export const loadFavoritesSaga = function*() {
+  try {
+    const userId = yield select(getLoggedInUserId);
+    const favorites = yield(call(fetchFavorites, userId));
+    yield put(favoritesRequestSuccess(favorites));
+  } catch (err) {
+    // TODO: handle error
+  }
+};
