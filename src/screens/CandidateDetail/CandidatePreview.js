c0d6eec46db61cb4c8f4d6fd694e80c58fd29ecb
Vanessa <vahurtad@ucsc.edu>
2018-08-17 18:43:58 -0700

    add button from favorites to candidate detail

--- src/screens/CandidateDetail/CandidatePreview.js
+++ src/screens/CandidateDetail/CandidatePreview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View,Text } from 'react-native';
+import {TouchableOpacity,View,Text } from 'react-native';
 import styles from '../styles';
 import { Avatar } from 'react-native-elements';
 
@@ -8,16 +8,21 @@ const FavoritesScreen = props =>(
   <View style={styles.container}>
     {props.candidates.map(candidate => (
       <View style={styles.container} key={candidate.id}>
-        <Avatar xlarge rounded source={{ uri: candidate.image}}/>
-        <Text >{candidate.name}</Text>
+        <TouchableOpacity onPress={props.goToCandidateDetail(candidate.id)}> 
+          <Avatar xlarge rounded source={{ uri: candidate.image}}/>
+        </TouchableOpacity>
+        <Text style={styles.nameText} >{candidate.name}</Text>
       </View>
     ))}   
 
   </View>);
 
 FavoritesScreen.propTypes = {
+  candidates: PropTypes.array,
+  favorites: PropTypes.bool,
+  name: PropTypes.string,
+  imageURI: PropTypes.string,
   goToCandidateDetail: PropTypes.func,
-  candidates: PropTypes.array
 };
 
 export default FavoritesScreen;
