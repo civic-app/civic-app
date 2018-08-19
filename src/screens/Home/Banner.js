7af23ca4d58993ea506e3065a2fca5dbdb50243f
gmferland <gmferland@gmail.com>
2018-08-19 14:40:43 -0400

    touch ups

--- src/screens/Home/Banner.js
+++ src/screens/Home/Banner.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import colors from '../../styles/colors';
 
 const Banner = props => {
   return (
     <View style={[styles.container, props.style, { backgroundColor: props.color }]}>
-      <Text style={styles.title}>{props.title}</Text>
-      <Text style={styles.subtitle}>{props.subtitle}</Text>
-      <Entypo name={props.icon} color={colors.white} size={60} style={styles.icon} />
+      <TouchableHighlight onPress={props.onPress}>
+        <View>
+          <Text style={styles.title}>{props.title}</Text>
+          <Text style={styles.subtitle}>{props.subtitle}</Text>
+          <Entypo name={props.icon} color={colors.white} size={60} style={styles.icon} />
+        </View>
+      </TouchableHighlight>
     </View>
   );
 };
@@ -18,7 +22,7 @@ Banner.propTypes = {
   color: PropTypes.string,
   icon: PropTypes.string,
   title: PropTypes.string,
-  style: PropTypes.object,
+  style: PropTypes.any,
   subtitle: PropTypes.string,
   onPress: PropTypes.func,
 };
