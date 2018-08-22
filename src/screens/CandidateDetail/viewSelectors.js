448a6177581d30f97b93614f406958b040e414b4
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-08-21 23:06:10 -0400

    Data for candidate summary

--- src/screens/CandidateDetail/viewSelectors.js
+++ src/screens/CandidateDetail/viewSelectors.js
@@ -5,11 +5,14 @@ import { Category } from '../../favorites/models';
 export const getCandidateSummary = (state, candidateId) => {
   const candidate = getCandidate(state, candidateId);
   const isFavorite = getIsFavorite(state, candidateId, Category.Candidates);
+  const matchPercent = 98 // TODO
   return candidate && {
+    id: candidate.id,
     name: candidate.name,
     imageURI: candidate.image,
-    isFavorite,
+    positions: candidate.electionIds,
     partyPreference: candidate.partyPreference,
-    id: candidate.id,
+    isFavorite,
+    matchPercent,
   };
 };
