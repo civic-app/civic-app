769a5c87d7e1d568fac00baf0f54f7457ab0a948
Vanessa <>
2018-08-27 01:55:39 -0700

    fixing small favorites ui

--- src/screens/CandidateDetail/CandidatePreviewContainer.js
+++ src/screens/CandidateDetail/CandidatePreviewContainer.js
@@ -2,28 +2,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { compose, lifecycle } from 'recompose'
 import CandidatePreview from './CandidatePreview'
-import { getCandidates, loadCandidates } from '../../candidate/redux/candidates'
-import { loadFavorites } from '../../favorites/redux';
+import { getCandidates, loadCandidates, getCandidate } from '../../candidate/redux/candidates'
+import { loadFavorites, getIsFavorite,toggleFavorite } from '../../favorites/redux';
+import { Category } from '../../favorites/models';
+
+export const getFavoriteCandidateData  = (state) => {
+  const candidates = getCandidates(state,toListCandidateMapper);
+  var isFavorite = candidates.map(candidate => {getIsFavorite(state,candidate.id,Category.Candidates)}) 
+  return candidates   
+}
 
 const Container = compose(
   connect(
-    (state) => ({
-      candidates: getCandidates(state, toListCandidateMapperPlaceholder
-      )}
-    ),
-    { loadCandidates, loadFavorites }
-  ),
-  lifecycle({
-    componentDidMount() {
-      this.props.loadCandidates();
-      this.props.loadFavorites();
-    }
-  })
-)(CandidatePreview);
-
-Container.PropTypes = {
-  candidateId: PropTypes.string,
-};
+    (state,ownprops) => ({
+      data: getFavoriteCandidateData(state,ownprops.candidateId)}),
+      {loadCandidates,loadFavorites}
+    ),  
+    lifecycle({
+      componentDidMount() {
+        this.props.loadCandidates();
+        this.props.loadFavorites();
+      }
+    })
+)(CandidatePreview)
 
 export default Container;
 
@@ -31,7 +32,8 @@ const toListCandidateMapper = candidate => ({
   name: candidate.name, 
   id: candidate.id,
   image: candidate.image,
-  favorite: candidate,
-  electionIds:candidate.electionIds 
+  electionIds:candidate.electionIds,
+  partyPreference: candidate.partyPreference,
+ 
 })
 
