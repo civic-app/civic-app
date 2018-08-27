718dcf310bc794bf3ae824ee939775365e60e087
Vanessa <>
2018-08-27 01:55:39 -0700

    fixing small things

--- src/screens/CandidateDetail/CandidatePreviewContainer.js
+++ src/screens/CandidateDetail/CandidatePreviewContainer.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { compose, lifecycle } from 'recompose'
 import CandidatePreview from './CandidatePreview'
 import { getCandidates, loadCandidates } from '../../candidate/redux/candidates'
-import { loadFavorites } from '../../favorites/redux';
+import { loadFavorites, getIsFavorite } from '../../favorites/redux';
+import { Category } from '../../favorites/models';
+
+export const getFavoriteCandidateData  = (state) => {
+  const candidates = getCandidates(state,toListCandidateMapper);
+  return candidates.map(candidate => {getIsFavorite(state,candidate.id,Category.Candidates)});
+};
 
 const Container = compose(
   connect(
-    (state) => ({
-      candidates: getCandidates(state, toListCandidateMapper
-      )}
+    (state, ownprops) => ({
+      data: getFavoriteCandidateData(state,ownprops.candidateId)}
     ),
-    { loadCandidates, loadFavorites }
+    { loadCandidates,loadFavorites },
   ),
   lifecycle({
     componentDidMount() {
@@ -31,7 +36,6 @@ const toListCandidateMapper = candidate => ({
   name: candidate.name,
   id: candidate.id,
   image: candidate.image,
-  favorite: candidate.isFavorite,
-  electionIds:candidate.electionIds
+  electionIds:candidate.electionIds,
+  partyPreference: candidate.partyPreference,
 });
-
