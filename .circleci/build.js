ab70f251931813f9a37f1c60629eb91b0e66088e
gmferland <gmferland@gmail.com>
2018-09-10 22:02:09 -0400

    don't even try to login anymore

--- .circleci/build.js
+++ .circleci/build.js
@@ -22,16 +22,9 @@ console.log('Installing Expo CLI');
 
 runCommand(exec('sudo npm install -g exp'))
   .then(() => {
-    console.log('Logging into Expo');
-    return runCommand(
-      exec(`exp login --non-interactive -u ${process.env.EXPO_EMAIL_ADDRESS} -p ${process.env.EXPO_PASSWORD}`),
-    );
-  })
-  .then(() => {
     console.log('Building app for Android');
     return runCommand(exec('exp --non-interactive build:android'));
   })
   .then(() => {
-    console.log('Build successfull. Logging out of Expo');
-    return runCommand(exec('exp --non-interactive logout'));
+    console.log('Build successfull!');
   });
