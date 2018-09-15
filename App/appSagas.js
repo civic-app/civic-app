52c190adaee801590a54e80b9e09e1d88efb6f98
Chandra Galbiati <chandra.galbiati@gmail.com>
2018-09-15 17:21:33 -0400

    Fetch all user data together

--- App/appSagas.js
+++ App/appSagas.js
@@ -1,9 +1,14 @@
 import { all } from 'redux-saga/effects';
 import authSaga from '../src/auth/sagas';
+import userSaga from '../src/user/sagas';
 import electionsScreenSaga from '../src/screens/Elections/sagas';
 
-function* appSaga() {
-  yield all([authSaga(), electionsScreenSaga()]);
-}
+const appSaga = function*() {
+  yield all([
+    authSaga(),
+    electionsScreenSaga(),
+    userSaga(),
+  ]);
+};
 
 export default appSaga;
---
+++ src/user/sagas.js
@@ -0,0 +1,38 @@
+import { call, select, put, takeEvery } from 'redux-saga/effects';
+import { getLoggedInUserId } from '../auth/redux';
+import { logOutSaga } from '../auth/sagas';
+import { fetchUser } from './api';
+
+export const loadUserDataSaga = function*() {
+  try {
+    const userId = yield select(getLoggedInUserId);
+    if (userId) {
+      const { responses, favorites, ...user } = yield call(fetchUser, userId);
+      yield put(userFetchSuccess(responses, favorites, user))
+    } else {
+      yield call(logOutSaga)
+    }
+
+  } catch (err) {
+    // TODO: handle error
+  }
+};
+
+export const userFetchSuccess = (responses, favorites, user) => ({
+  type: UserActionType.RequestSuccess,
+  payload: { responses, favorites, user },
+});
+
+export const loadUser = (force = false) => ({
+  type: UserActionType.Request,
+  payload: force
+});
+
+export const UserActionType = {
+  Request: 'civicApp/user/REQUEST',
+  RequestSuccess: 'civicApp/user/REQUEST_SUCCESS',
+};
+
+export default function*() {
+  yield takeEvery(UserActionType.Request, loadUserDataSaga);
+}
