dfe0a4609eedde3ac7d3624063bd025080ff0cc9
William Chu <Williamchu51@gmail.com>
2018-10-10 18:31:39 -0700

    add styling for banner and alert

--- src/screens/Home/VoterRegAlert.js
+++ src/screens/Home/VoterRegAlert.js
@@ -8,17 +8,21 @@ const VoterRegAlert = props => {
   return props.isUserRegistered ? null : (
     <View style={styles.container}>
       <Text style={styles.title}>Are you registered to vote?</Text>
-      <View style={styles.buttonContainer}>
+      <View style={styles.buttonGroup}>
         <Button
           buttonStyle={styles.button}
+          containerViewStyle={styles.buttonContainer}
+          borderRadius={3}
           textStyle={styles.buttonText}
           title="Yes"
           onPress={props.onYesButtonPress}
         />
         <Button
           buttonStyle={styles.button}
+          containerViewStyle={styles.buttonContainer}
+          borderRadius={3}
           textStyle={styles.buttonText}
-          title="No/Not Sure"
+          title="No / Not Sure"
           onPress={props.onNoButtonPress}
         />
       </View>
@@ -34,36 +38,53 @@ VoterRegAlert.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     alignSelf: 'stretch',
     backgroundColor: colors.red,
-    minHeight: 100,
-    padding: 16,
-    borderRadius: 2,
-    shadowColor: colors.black,
+    paddingTop: 10,
+    borderRadius: 3,
+    elevation: 2,
+    shadowColor:colors.black,
     shadowOffset: {
-      width: 0,
-      height: 1,
+      width: 2, height: 2
     },
-    shadowOpacity: 0.2,
     shadowRadius: 3,
-    margin: 8,
+    shadowOpacity: 0.2,
+    margin: 10,
+    marginBottom: 0,
   },
   title: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: '500',
+    textAlign: 'center',
+  },
+  buttonGroup: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flex: 1,
+    elevation: 2,
+    borderRadius: 3,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+    marginBottom: 20,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
   },
   button: {
     backgroundColor: colors.white,
-    width: 130,
-    height: 40,
-    margin: 8,
-    padding: 8,
   },
   buttonText: {
     color: colors.black,
